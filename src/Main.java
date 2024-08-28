c5d6302989e6bf1330075d9e93ecf01411b63b5b
Nicholas-afk <64998158+Nicholas-afk@users.noreply.github.com>
2024-08-28 09:34:49 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Nicholas Tanner
 *****************************************************************************************************
 */
 
@@ -12,9 +12,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        int age;
+        String firstName;
+        String favouriteFood;
 
+        System.out.println("Please enter your age: ");
+        age = scanner.nextInt();
+
+        System.out.println("Please enter your first name: ");
+        firstName = scanner.next();
+
+        System.out.println("Please enter your favourite food: ");
+        favouriteFood = scanner.next();
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Age: " + age);
+        System.out.println("Favourite food: " + favouriteFood);
 
     }
 
 }
+
